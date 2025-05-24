ec9e3d73d424754e57a475bdc964aabda72a2af9
Dmitry Sulman <dmitry.sulman@gmail.com>
2025-05-24 15:44:01 +0300

    Continue worker on starter
    Signed-off-by: Dmitry Sulman <dmitry.sulman@gmail.com>

--- logback-access-reactor-netty-spring-boot-starter/build.gradle.kts
+++ logback-access-reactor-netty-spring-boot-starter/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("conventions")
+    alias(libs.plugins.kotlin.springPlugin)
+    kotlin("kapt")
 }
 
 description = "Spring Boot Starter for Logback Access integration with Reactor Netty"
@@ -7,5 +9,13 @@ description = "Spring Boot Starter for Logback Access integration with Reactor N
 dependencies {
     api(project(":logback-access-reactor-netty"))
 
+    implementation(libs.spring.boot.starter)
     implementation(libs.slf4j.api)
+
+    provided(libs.spring.boot.starter.reactorNetty)
+
+    kapt(libs.spring.boot.autoconfigureProcessor)
+    kapt(libs.spring.boot.configurationProcessor)
+
+    testImplementation(libs.spring.boot.starter.test)
 }
---
+++ logback-access-reactor-netty-spring-boot-starter/src/main/kotlin/io/github/dmitrysulman/logback/access/reactor/netty/joran/ReactorNettyJoranConfigurator.kt
@@ -0,0 +1,7 @@
+package io.github.dmitrysulman.logback.access.reactor.netty.joran
+
+import ch.qos.logback.access.common.joran.JoranConfigurator
+
+class ReactorNettyJoranConfigurator : JoranConfigurator() {
+
+}
