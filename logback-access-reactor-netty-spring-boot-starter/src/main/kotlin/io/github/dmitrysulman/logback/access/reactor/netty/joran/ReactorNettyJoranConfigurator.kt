2296bda945f2a35279cd672f57f4e5bf2411c069
Dmitry Sulman <dmitry.sulman@gmail.com>
2025-05-24 18:35:48 +0300

    Continue worker on starter
    Signed-off-by: Dmitry Sulman <dmitry.sulman@gmail.com>

--- logback-access-reactor-netty-spring-boot-starter/src/main/kotlin/io/github/dmitrysulman/logback/access/reactor/netty/joran/ReactorNettyJoranConfigurator.kt
+++ logback-access-reactor-netty-spring-boot-starter/src/main/kotlin/io/github/dmitrysulman/logback/access/reactor/netty/joran/ReactorNettyJoranConfigurator.kt
@@ -1,7 +1,8 @@
 package io.github.dmitrysulman.logback.access.reactor.netty.joran
 
 import ch.qos.logback.access.common.joran.JoranConfigurator
+import org.springframework.core.env.Environment
 
-class ReactorNettyJoranConfigurator : JoranConfigurator() {
-
-}
+class ReactorNettyJoranConfigurator(
+    environment: Environment,
+) : JoranConfigurator()
