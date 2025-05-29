c64d32d88cdfcceb2a357caacd2b42f208500245
Dmitry Sulman <dmitry.sulman@gmail.com>
2025-05-29 22:29:02 +0300

    Start Joran Configurator
    Signed-off-by: Dmitry Sulman <dmitry.sulman@gmail.com>

--- logback-access-reactor-netty-spring-boot-starter/src/main/kotlin/io/github/dmitrysulman/logback/access/reactor/netty/joran/ReactorNettyJoranConfigurator.kt
+++ logback-access-reactor-netty-spring-boot-starter/src/main/kotlin/io/github/dmitrysulman/logback/access/reactor/netty/joran/ReactorNettyJoranConfigurator.kt
@@ -1,8 +1,38 @@
 package io.github.dmitrysulman.logback.access.reactor.netty.joran
 
 import ch.qos.logback.access.common.joran.JoranConfigurator
+import ch.qos.logback.core.joran.spi.ElementSelector
+import ch.qos.logback.core.joran.spi.RuleStore
+import ch.qos.logback.core.model.Model
+import ch.qos.logback.core.model.processor.DefaultProcessor
 import org.springframework.core.env.Environment
+import java.util.function.Supplier
 
 class ReactorNettyJoranConfigurator(
-    environment: Environment,
-) : JoranConfigurator()
+    private val environment: Environment,
+) : JoranConfigurator() {
+    override fun addElementSelectorAndActionAssociations(rs: RuleStore) {
+        super.addElementSelectorAndActionAssociations(rs)
+        rs.addRule(ElementSelector("*/springProfile"), ::SpringProfileAction)
+        rs.addTransparentPathPart("springProfile")
+    }
+
+    override fun sanityCheck(topModel: Model) {
+        super.sanityCheck(topModel)
+    }
+
+    override fun addModelHandlerAssociations(defaultProcessor: DefaultProcessor) {
+        defaultProcessor.addHandler(SpringProfileModel::class.java) { _, _ ->
+            SpringProfileModelHandler(context, environment)
+        }
+        super.addModelHandlerAssociations(defaultProcessor)
+    }
+
+    override fun buildModelInterpretationContext() {
+        super.buildModelInterpretationContext()
+        modelInterpretationContext.configuratorSupplier =
+            Supplier {
+                ReactorNettyJoranConfigurator(environment).also { it.context = this.context }
+            }
+    }
+}
