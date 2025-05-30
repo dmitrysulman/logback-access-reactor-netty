5c5e478d832352e3dfd8b6b2a324066d038dd248
Dmitry Sulman <dmitry.sulman@gmail.com>
2025-05-30 19:31:59 +0300

    Add documentation to Joran classes
    Signed-off-by: Dmitry Sulman <dmitry.sulman@gmail.com>

--- logback-access-reactor-netty-spring-boot-starter/src/main/kotlin/io/github/dmitrysulman/logback/access/reactor/netty/joran/LogbackAccessSpringProfileModelHandler.kt
+++ logback-access-reactor-netty-spring-boot-starter/src/main/kotlin/io/github/dmitrysulman/logback/access/reactor/netty/joran/LogbackAccessSpringProfileModelHandler.kt
@@ -7,6 +7,14 @@ import ch.qos.logback.core.model.processor.ModelInterpretationContext
 import ch.qos.logback.core.util.OptionHelper
 import org.springframework.core.env.Environment
 
+/**
+ * Logback Access [ModelHandlerBase] model handler to support `<springProfile>` tags.
+ *
+ * See [SpringProfileModelHandler](https://github.com/spring-projects/spring-boot/blob/main/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/logging/logback/SpringProfileModelHandler.java).
+ *
+ * @see [LogbackAccessSpringProfileModel]
+ * @see [LogbackAccessSpringProfileAction]
+ */
 class LogbackAccessSpringProfileModelHandler(
     context: Context,
     private val environment: Environment,
