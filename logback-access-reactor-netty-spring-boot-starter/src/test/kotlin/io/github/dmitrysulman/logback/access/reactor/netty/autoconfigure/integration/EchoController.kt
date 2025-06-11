2ef36736de9eb73397dc7689ffb50895455a2893
Dmitry Sulman <dmitry.sulman@gmail.com>
2025-06-11 09:12:03 +0300

    Improve integration tests
    Signed-off-by: Dmitry Sulman <dmitry.sulman@gmail.com>

--- logback-access-reactor-netty-spring-boot-starter/src/test/kotlin/io/github/dmitrysulman/logback/access/reactor/netty/autoconfigure/integration/EchoController.kt
+++ logback-access-reactor-netty-spring-boot-starter/src/test/kotlin/io/github/dmitrysulman/logback/access/reactor/netty/autoconfigure/integration/EchoController.kt
@@ -1,5 +1,6 @@
 package io.github.dmitrysulman.logback.access.reactor.netty.autoconfigure.integration
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -9,5 +10,5 @@ class EchoController {
     @GetMapping("/get")
     fun getRequest(
         @RequestParam param: String,
-    ): String = param
+    ): ResponseEntity<String> = ResponseEntity.status(200).header("response_header", "response_header_value").body(param)
 }
--- logback-access-reactor-netty-spring-boot-starter/src/test/kotlin/io/github/dmitrysulman/logback/access/reactor/netty/autoconfigure/integration/IntegrationTests.kt
+++ logback-access-reactor-netty-spring-boot-starter/src/test/kotlin/io/github/dmitrysulman/logback/access/reactor/netty/autoconfigure/integration/IntegrationTests.kt
@@ -2,10 +2,13 @@ package io.github.dmitrysulman.logback.access.reactor.netty.autoconfigure.integr
 
 import io.github.dmitrysulman.logback.access.reactor.netty.EventCaptureAppender
 import io.github.dmitrysulman.logback.access.reactor.netty.ReactorNettyAccessLogFactory
+import io.kotest.matchers.comparables.shouldBeGreaterThan
+import io.kotest.matchers.longs.shouldBeGreaterThanOrEqual
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest(
@@ -15,13 +18,16 @@ import org.springframework.test.web.reactive.server.WebTestClient
 class IntegrationTests(
     @Autowired private val webTestClient: WebTestClient,
     @Autowired private val reactorNettyAccessLogFactory: ReactorNettyAccessLogFactory,
+    @LocalServerPort private val localServerPort: Int,
 ) {
     @Test
     fun `smoke test`() {
+        val now = System.currentTimeMillis()
         val value = "test"
         webTestClient
             .get()
             .uri("/get?param={param}", value)
+            .header("request_header", "request_header_value")
             .exchange()
             .expectStatus()
             .isOk
@@ -33,5 +39,19 @@ class IntegrationTests(
             reactorNettyAccessLogFactory.accessContext.getAppender("CAPTURE") as EventCaptureAppender
 
         eventCaptureAppender.list.size shouldBe 1
+        val accessEvent = eventCaptureAppender.list.first()
+        accessEvent.requestURL shouldBe "GET /get?param=$value HTTP/1.1"
+        accessEvent.contentLength shouldBe value.length
+        accessEvent.localPort shouldBe localServerPort
+        accessEvent.requestURI shouldBe "/get"
+        accessEvent.queryString shouldBe "?param=$value"
+        accessEvent.protocol shouldBe "HTTP/1.1"
+        accessEvent.method shouldBe "GET"
+        accessEvent.statusCode shouldBe 200
+        accessEvent.elapsedTime shouldBeGreaterThanOrEqual 0
+        accessEvent.timeStamp shouldBeGreaterThan now
+        accessEvent.remoteUser shouldBe "-"
+        accessEvent.getRequestHeader("request_header") shouldBe "request_header_value"
+        accessEvent.getResponseHeader("response_header") shouldBe "response_header_value"
     }
 }
