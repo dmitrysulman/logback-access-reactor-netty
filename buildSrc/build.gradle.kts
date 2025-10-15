9722659b717330758b8867ecca3e317ea703d307
Dmitry Sulman <dmitry.sulman@gmail.com>
2025-10-15 16:35:27 +0300

    Upgrade dependencies
    Signed-off-by: Dmitry Sulman <dmitry.sulman@gmail.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,11 +16,6 @@ dependencies {
     // https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 
-    // Override the old Jackson version that comes with dokka
-    // to fix READ_DATE_TIMESTAMPS_AS_NANOSECONDS on Jreleaser tasks
-    // https://github.com/dependency-check/DependencyCheck/issues/6192
-    implementation(platform(libs.jackson.bom))
-
     implementation(libs.dokka.javadoc.plugin)
     implementation(libs.dokka.plugin)
     implementation(libs.kotlin.plugin)
